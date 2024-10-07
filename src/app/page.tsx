5d6c93287c4025ed04473346e8738c834f1b569d
a.khodaiedoust <a.khodaiedoust@emofid.com>
2024-10-07 23:48:57 +0330

    fix(configs): lint-staged config fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,6 @@ export default function Home() {
           height={38}
           priority
         />
-       
 
         <div className="flex flex-col items-center gap-4 sm:flex-row">
           <a
