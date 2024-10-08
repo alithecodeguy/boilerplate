699912f531394a5524712b29e5e32dd9c7ac7848
a.khodaiedoust <a.khodaiedoust@emofid.com>
2024-10-08 21:13:29 +0330

    feat(styles): tw-colors plugin added to tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,76 +1,19 @@
-import Image from 'next/image';
-
 export default function Home() {
   return (
-    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
-      <main className="row-start-2 flex flex-col items-center gap-8 sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-
-        <div className="flex flex-col items-center gap-4 sm:flex-row">
-          <a
-            className="flex h-10 items-center justify-center gap-2 rounded-full border border-solid border-transparent bg-foreground px-4 text-sm text-background transition-colors hover:bg-[#383838] sm:h-12 sm:px-5 sm:text-base dark:hover:bg-[#ccc]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Deploy now
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex flex-wrap items-center justify-center gap-6">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <div className="h-dvh w-dvw bg-primary_test font-[family-name:var(--font-geist-sans)]">
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi, praesentium consectetur
+      voluptates, accusamus et alias, quos tempora saepe eum voluptatum distinctio? Voluptas
+      consequuntur provident libero cum, quae corrupti repudiandae! Provident explicabo unde earum,
+      excepturi culpa perspiciatis numquam necessitatibus, dolores doloribus nihil est inventore
+      rerum voluptatum consequatur dolorum, nesciunt minima odit commodi. Incidunt ad natus commodi
+      neque provident labore. Tempora necessitatibus pariatur tenetur amet modi perferendis aliquam,
+      dignissimos, quo magni repudiandae officiis sequi non ipsum maxime mollitia nulla? Excepturi
+      dolor, ut cumque totam impedit, inventore architecto quidem eum deserunt neque nobis hic
+      perspiciatis natus cum provident at, sed esse repellat ratione!
     </div>
   );
+}
+
+{
+  /* <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20"> */
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,24 @@
+import { createThemes } from 'tw-colors';
+
+// types
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
-  theme: {
-    extend: {
-      colors: {
-        background: 'var(--background)',
-        foreground: 'var(--foreground)',
+  theme: {},
+  plugins: [
+    createThemes({
+      light: {
+        primary_test: '#ccc'
       },
-    },
-  },
-  plugins: [],
+      dark: {
+        primary_test: '#333'
+      }
+    })
+  ]
 };
 export default config;
